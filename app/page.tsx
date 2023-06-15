a789f443cbb7f238b282e4a486a24d21bba7a2aa
Jeevan-Durgam <jeevan.durgam634@gmail.com>
2023-06-15 14:22:43 +0530

    video added

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import AboutMe from "./components/AboutMe";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import ContactForm from "./components/ContactForm";
+import VideoLoader from "./components/VideoLoader";
 
 export default function Home() {
   return (
     <main className="">
-      {/* <VideoLoader /> */}
+      <VideoLoader />
       <Navbar />
       <Hero />
       <AboutMe />
